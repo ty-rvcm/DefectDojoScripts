1b696ab31312cdfadd679e7c8054274777c9e960
Ty Bross <ty.bross@cms.hhs.gov>
2023-07-05 15:06:08 -0700

    latest updates

--- defectdojo-users.py
+++ defectdojo-users.py
@@ -7,11 +7,26 @@ def main():
         logging.error("DEFECTDOJO_API_KEY environmnent variable not populated. Exiting.")
         exit()
     api_key = os.environ['DEFECTDOJO_API_KEY']
-
+    
     headers = {
         'content-type': 'application/json',
         'Authorization': f"Token {api_key}"
     }
+
+    proxies = {"http":"http://127.0.0.1:8080", "https":"http://127.0.0.1:8080"}
+
+    users_url = "https://defectdojo.batcave.internal.cms.gov/api/v2/users/?limit=100"
+
+    r = requests.get(users_url, headers=headers)
+
+    # Figure out using python requests documentation at https://pypi.org/project/requests/ how to get the JSON of the response
+    print(r) #<---- answer goes into the parentheses
+
+
+    
+
+# r = requests.get('https://httpbin.org/basic-auth/user/pass'
+
 
 if __name__ == "__main__":
     main()
