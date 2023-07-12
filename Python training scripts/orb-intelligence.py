c10bc73f781a09513ce71355d7427b552858e845
Ty Bross <ty.bross@cms.hhs.gov>
2023-07-12 14:54:00 -0700

    new commit

--- Python training scripts/orb-intelligence.py
+++ Python training scripts/orb-intelligence.py
@@ -7,11 +7,14 @@ proxies = {"http":"http://127.0.0.1:8080", "https":"http://127.0.0.1:8080"}
 
 # Get company info by name
 def getCompanyByName():
-    r = requests.get("https://api.orb-intelligence.com/3/match/?api_key=c66c5dad-395c-4ec6-afdf-7b78eb94166a&name=nike&country=us", proxies=proxies, verify=False)
-
+    requestURL = "https://api.orb-intelligence.com/3/match/?api_key=c66c5dad-395c-4ec6-afdf-7b78eb94166a&name=nike&country=us"
+    
     """ 
-    Additional logic goes here
-    Challenge: Print out the orb number and company name only if the address1 field = "1 BOWERMAN DR" in Beaverton, OR
+    The task:
+    1. Create the URL to pull down the information from the request URL
+    2. Print out the orb number and company name only if the address1 field = "1 BOWERMAN DR" in Beaverton, OR
+
+    Objectives: Understand common processing logic and working with JSON objects in Python
     """
 
 if __name__ == "__main__":
