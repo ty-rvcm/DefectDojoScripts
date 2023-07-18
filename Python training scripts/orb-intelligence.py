9f78d32b7bdcfb3b4b809d7c3fd5652cbd0a9c6c
OhhLenzo <foster.brendan01@gmail.com>
2023-07-18 09:27:55 -1000

    add orb num and company name

--- Python training scripts/orb-intelligence.py
+++ Python training scripts/orb-intelligence.py
@@ -7,47 +7,51 @@ proxies = {"http":"http://127.0.0.1:8080", "https":"http://127.0.0.1:8080"} #dic
 
 # Get company info by name
 def getCompanyByName():
-    requestURL = "https://api.orb-intelligence.com/3/match/?api_key=c66c5dad-395c-4ec6-afdf-7b78eb94166a&name=nike&country=us"
+    requestURL = "https://api.orb-intelligence.com/3/match/?api_key={API_KEY}&name=nike&country=us"
 
-    # build request object here
-    # request methods:
-    # GET, POST, PUT, DELETE, TRACE, PATCH, UPDATE, etc...
-    r = requests.get(requestURL)
+    try:
 
-    if r.status_code == 200: 
-        data=r.json()
-        
-        response_data = data["results"]
-        
-    
-        
-        
-    
-    """
-    request_fields
-    results_count
-    results
-    """
+        # Object built right here
+        r = requests.get(requestURL, proxies=proxies)
+        # Checks if the request was successful
+        if r.status_code == 200: 
+            data=r.json()
+
+            response_data = data["results"]
+
+            found_match = False
+            
+            # Extract relevant information based on condition
+            for result in response_data:
+                address1 = result.get('address1')
+                city = result.get('city')
+                state = result.get('state')
+
+                if address1 == '1 BOWERMAN DR' and city == 'Beaverton' and state == 'OR':
+                    orb_number = result.get('orb_number')
+                    company_name = result.get('name')
+                    print(f"ORB Number: {orb_number}")
+                    print(f"Company Name: {company_name}")
+                    found_match = True
+            
+            if not found_match:
+                print("No matching company found.")
+        else:
+            print("Request failed with status code:", r.status_code)
+    except requests.RequestException as e:
+        print("An error has occured during the request:", e)
+
+    if __name__ == "__main__":
+        getCompanyByName()
+
     
     """ 
     The task:
     1. Create the request object to pull down the information from the request URL - DONE
-        - How do you know the request was successful? - 200 OK response
+        - How do you know the request was successful? 
+            - 200 OK response
     2. Use the API_KEY parameter in the request in place of the hardcoded key in the URL -  concatenation of strings/paramaters
     3. Print out the orb number and company name only if the address1 field = "1 BOWERMAN DR" in Beaverton, OR
 
-    Data types:
-    1. character: 'a'
-    2. integer: 123
-    3. long: 23423.12
-    4. "This is a string"
-    5. dictionary
-    6. array : fruit=["apple","pear"]
-    7. booleans
-    8. list = ArrayList
-
     Objectives: Understand common processing logic and working with JSON objects in Python
     """
-
-if __name__ == "__main__":
-    getCompanyByName()
